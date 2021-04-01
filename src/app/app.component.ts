53cd87bc8e426df692e47e12956931351a2751da
lakarimori <arimoriakio@gmail.com>
2021-03-31 21:45:29 -0300

    adding navigation - lesson 87

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  selectedFeature: string = 'recipe';
+
+  onNavigate(feature: string) {
+    this.selectedFeature = feature;
+  }
+}
